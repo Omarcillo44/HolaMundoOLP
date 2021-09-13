880500cb6a389e6ce0ca31dc187075cdfa8bbd24
Omar Lorenzo Pacheco <omarlorenz44@gmail.com>
2021-09-13 11:36:48 -0500

    Lorenzo_Pacheco_Omar 5IM9 Hola mundo

---
+++ app/src/main/java/ipn/cecyt9/holamundoolp/MainActivity.java
@@ -0,0 +1,13 @@
+package ipn.cecyt9.holamundoolp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
